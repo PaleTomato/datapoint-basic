2062b0758e165af8ac4e67714047013d6c71dbc3
Patrick Leedham <ptl76@hotmail.co.uk>
2017-05-01 16:37:23 +0100

    Added functions to __init__.py

---
+++ datapointbasic/__init__.py
@@ -0,0 +1,31 @@
+import datapointbasic.searchtools
+import datapointbasic.current_conditions
+
+def placesearch(api_key=""):
+    """
+    Function placesearch creates a new instance of the datapointbasic
+    LocationSearch class, and returns it. The methods of the LocationSearch can
+    then be used to search for locations in the Met Office DataPoint.
+    
+    The single input required is a valid API key for DataPoint.
+    The function returns a LocationSearch object.
+    """
+    
+    search_obj = datapointbasic.searchtools.LocationSearch(api_key=api_key)
+    
+    return search_obj
+
+def current_conditions(api_key=""):
+    """
+    Function current_conditions creates a new instance of the datapointbasic
+    CurrentConditions class, and returns it. The methods of CurrentConditions
+    can then be used to obtain the current weather conditions for a specific
+    location
+    
+    The single input required is a valid API key for DataPoint.
+    The function returns a CurrentConditions object.
+    """
+    
+    search_obj = datapointbasic.searchtools.LocationSearch(api_key=api_key)
+    
+    return search_obj
