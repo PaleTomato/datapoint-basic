1f29c0ae55efb270b29d269f3d7b8a133c0d1eb2
Patrick Leedham <ptl76@hotmail.co.uk>
2017-06-01 19:34:01 +0100

    Updated setup.py

--- setup.py
+++ setup.py
@@ -3,8 +3,12 @@ from setuptools import setup
 setup(name='datapoint-basic',
       version='0.2',
       description='Simple access to Met Office\s datapoint API',
-      url='https://github.com/PaleTomato/datapoint-simple',
+      url='https://github.com/PaleTomato/datapoint-basic',
       author='Patrick Leedham',
       author_email='ptl76@hotmail.co.uk',
       license='GPL-3.0',
-      packages=['datapoint'])
+      packages=['datapointbasic'],
+      install_requires=[
+          'datapoint',
+          'math'
+      ])
