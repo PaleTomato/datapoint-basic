9656d63389ee76ad84ce396b2036f689b480dd50
Patrick Leedham <ptl76@hotmail.co.uk>
2017-06-19 11:14:01 +0100

    Set properties such as temperature from methods to attributes

--- datapointbasic/forecast.py
+++ datapointbasic/forecast.py
@@ -54,6 +54,15 @@ class DayForecast(object):
         """
         self._forecast  = forecast
         
+        self.temperature           = self._get_values("temperature")
+        self.feelslike_temperature = self._get_values("feels_like_temperature")
+        self.precipitation         = self._get_values("precipitation")
+        self.humidity              = self._get_values("humidity")
+        self.uv_index              = self._get_values("uv")
+        self.weather_type          = self._get_text("weather")
+        self.wind_speed            = self._get_values("wind_speed")
+        self.wind_direction        = self._get_values("wind_direction")
+        
     
     def timesteps(self):
         """
@@ -69,127 +78,6 @@ class DayForecast(object):
         return times
         
         
-    def temperature(self):
-        """
-        Function to get the current temperature at the inputted location.
-    
-        The outputs are a list of numerical temperature values for each
-        timestep, and the units as a string.
-        """
-        weathertype = 'temperature'
-        
-        values = self._get_values(weathertype)
-        units  = self._get_units(weathertype)
-
-        return (values, units)
-    
-        
-    def feelslike_temperature(self):
-        """
-        Function to get the current feels-like temperature at the inputted
-        location.
-    
-        The outputs are a list of numerical feels-like temperature values for
-        each timestep, and the units as a string.
-        """
-        weathertype = 'feels_like_temperature'
-        
-        values = self._get_values(weathertype)
-        units  = self._get_units(weathertype)
-
-        return (values, units)
-        
-    
-    def precipitation(self):
-        """
-        Function to get the current precipitation at the inputted location.
-    
-        The outputs are a list of numerical precipitation values for each
-        timestep, and the units as a string.
-        """
-        weathertype = 'precipitation'
-        
-        values = self._get_values(weathertype)
-        units  = self._get_units(weathertype)
-
-        return (values, units)
-    
-    
-    def humidity(self):
-        """
-        Function to get the current humidity at the inputted location.
-    
-        The outputs are a list of numerical humidity values for each timestep,
-        and the units as a string.
-        """
-        weathertype = 'humidity'
-        
-        values = self._get_values(weathertype)
-        units  = self._get_units(weathertype)
-
-        return (values, units)
-        
-        
-    def uv_index(self):
-        """
-        Function to get the current uv index at the inputted location.
-    
-        The outputs are a list of numerical uv indices for each timestep, each
-        from 1 to 10.
-        """
-        weathertype = 'uv'
-        
-        values = self._get_values(weathertype)
-        
-        return values
-        
-    
-    def weather_type(self):
-        """
-        Function to get the current weather type at the inputted location.
-    
-        The output is a list of string descriptions of the weather for each
-        timestep e.g. 'Heavy Rain'. The weather codes are also outputted as a
-        list of integers.
-        """
-        weathertype = 'weather'
-        
-        text   = self._get_text(weathertype)
-        values = self._get_values(weathertype)
-        
-        return (text, values)
-            
-    
-    def wind_speed(self):
-        """
-        Function to get the current wind speed at the inputted location.
-    
-        The output is a list of numerical wind speeds for each timestep, and the
-        units as a string.
-        """
-        weathertype = 'wind_speed'
-        
-        values = self._get_values(weathertype)
-        units  = self._get_units(weathertype)
-        
-        return (values, units)
-        
-    
-    def wind_direction(self):
-        """
-        Function to get the current wind direction at the inputted location.
-    
-        The output is a list of string compass directions for each timestep. The
-        string denotes the direction from which the wind is coming, e.g. 'NW'
-        for a wind from the north-west.
-        """
-        weathertype = 'wind_direction'
-        
-        values = self._get_values(weathertype)
-        
-        return values
-        
-    
     def _get_text(self, weathertype):
         """
         Returns the text for the inputted weather type as a list of values for
