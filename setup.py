52c9b36289ed1c1ac0784460bfaf40062ba88efc
Patrick Leedham <ptl76@hotmail.co.uk>
2017-11-26 21:25:11 +0000

    Update version number

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='datapoint-basic',
-      version='0.3',
+      version='1.0.0',
       description='Simple access to Met Office\s datapoint API',
       url='https://github.com/PaleTomato/datapoint-basic',
       author='Patrick Leedham',
