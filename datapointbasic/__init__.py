e1088e87f0e614751a0a708a5be2537b8f546519
Patrick Leedham <ptl76@hotmail.co.uk>
2018-02-24 15:38:29 +0000

    Added function to enter API key at top level of module.

--- datapointbasic/__init__.py
+++ datapointbasic/__init__.py
@@ -1,2 +1,20 @@
 from .tools import ApiManager
 from .sitelist import SiteList
+
+def enter_api_key(api_key):
+    """
+    Function to enter and register a DataPoint API key.
+    
+    This function is used to enter a DataPoint API key to use when retrieving
+    data from the Met Office DataPoint service. The key is stored in an object
+    with a shared state, so that any requests to DataPoint will use the same
+    key.
+    
+    Inputs:
+    -------
+    api_key - Your API key that can be obtained by logging into your Met Office
+              DataPoint account. The key should be entered as a single string,
+              including any dashes.
+    """
+    
+    ApiManager(api_key)
