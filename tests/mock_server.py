a2fe6bc1c72baecb02afe8964ac0984ffad4ad1e
Patrick Leedham <ptl76@hotmail.co.uk>
2018-09-27 23:22:07 +0100

    Minor corrections to mock server and additional functions.

--- tests/mock_server.py
+++ tests/mock_server.py
@@ -12,18 +12,19 @@ class MockDataPointServer(BaseHTTPRequestHandler):
     def do_GET(self):
 
         parsed_path = parse.urlparse(self.path)
-        path = parsed_path.path # e.g. /public/data/val/wxfcs/all/json/capabilities
-        query = parsed_path.query # res=3hourly&key=abcde
+        path = parsed_path.path
+        query = parsed_path.query
         query_dict = dict(parse.parse_qsl(query))
         
         if query_dict['key'] != MOCK_API_KEY:
             self.send_response(403)
-            self.end_headers
+            self.end_headers()
             return
 
         
         # Process an HTTP GET request and return a response with an HTTP 200 status.
         self.send_response(200)
+        self.send_header('Content-type','application/json')
         self.end_headers()
         self.wfile.write(get_json_from_path(path, query_dict))
 
@@ -46,6 +47,20 @@ def get_json_from_path(path, query):
     
     return content
     
+def get_free_port():
+    s = socket.socket(socket.AF_INET, type=socket.SOCK_STREAM)
+    s.bind(('localhost', 0))
+    address, port = s.getsockname()
+    s.close()
+    return port
+
+
+def start_mock_server(port):
+    mock_server = HTTPServer(('localhost', port), MockDataPointServer)
+    mock_server_thread = Thread(target=mock_server.serve_forever)
+    mock_server_thread.setDaemon(True)
+    mock_server_thread.start()
+
 
 if __name__ == '__main__':
     server = HTTPServer(('localhost', 8080), MockDataPointServer)
