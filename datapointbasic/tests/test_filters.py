d79c415399af243d894367b6b79a896cb7689e1e
Patrick Leedham <ptl76@hotmail.co.uk>
2018-10-20 18:17:38 +0100

    Added test case for today filter

--- datapointbasic/tests/test_filters.py
+++ datapointbasic/tests/test_filters.py
@@ -7,7 +7,10 @@ from datapointbasic.tests.mock_server import MockDataPointRequest
 request_3hourly = MockDataPointRequest("val", "wxfcs", "all", "1234",
     {'res':'3hourly'})
 
-winddir_values = ["W", "W", "WSW", "WSW", "WSW", "WSW", "SSW", "SW","SE",
+all_values_times = [datetime(2018, 9, 21) + timedelta(minutes=delta)
+    for delta in range(720, 7200, 180)]
+
+all_values_winddir = ["W", "W", "WSW", "WSW", "WSW", "WSW", "SSW", "SW","SE",
     "ESE", "E", "ENE", "ENE", "NE", "NE", "N", "N", "NNW", "NNW", "NNW", "NNW",
     "NNW", "NNW", "NNE", "NNE", "NNE", "N", "NNE", "NNE", "NE", "NE", "E",
     "SE", "SE", "SE", "ESE"]
@@ -16,13 +19,15 @@ class TestFilters(unittest.TestCase):
 
     def test_filter_all(self):
         filter_all = filters.filter_all(request_3hourly)
-
-        start_time = datetime(2018, 9, 21)
-        times = [start_time + timedelta(minutes=delta) 
-            for delta in range(720, 7200, 180)]
         
+        self.assertEqual(filter_all.times, all_values_times)
+        self.assertEqual(filter_all["D"], all_values_winddir)
 
-        self.assertEqual(filter_all.get_all_times(), times)
-        self.assertEqual(filter_all.get_all_values("D"), winddir_values)
+    def test_filter_today(self):
+        filter_today = filters.filter_today(request_3hourly)
+
+        self.assertEqual(filter_today.times, all_values_times[:4])
+        self.assertEqual(filter_today["D"], all_values_winddir[:4])
+        
 
 
