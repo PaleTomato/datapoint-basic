7cde173b726b501cfb08012dfcd6f0576b76efd3
Patrick Leedham <ptl76@hotmail.co.uk>
2018-12-25 08:46:56 +0000

    Updated code to conform more to PEP8 standards

--- datapointbasic/tests/mock_server.py
+++ datapointbasic/tests/mock_server.py
@@ -5,9 +5,8 @@ import socket
 from threading import Thread
 from urllib import parse
 
-import requests
-
 MOCK_API_KEY = 'abcdefg'
+
 
 class MockDataPointServer(BaseHTTPRequestHandler):
     """
@@ -19,16 +18,15 @@ class MockDataPointServer(BaseHTTPRequestHandler):
         path = parsed_path.path
         query = parsed_path.query
         query_dict = dict(parse.parse_qsl(query))
-        
+
         if query_dict['key'] != MOCK_API_KEY:
             self.send_response(403)
             self.end_headers()
             return
 
-        
         # Return a response with an HTTP 200 status.
         self.send_response(200)
-        self.send_header('Content-type','application/json')
+        self.send_header('Content-type', 'application/json')
         self.end_headers()
         self.wfile.write(get_json_from_path(path, query_dict))
 
@@ -45,10 +43,10 @@ class MockDataPointRequest(object):
         Initialise the object
         """
 
-        self.val    = val
-        self.wx     = wx
-        self.item   = item
-        self.feed   = feed
+        self.val = val
+        self.wx = wx
+        self.item = item
+        self.feed = feed
         self.params = params
 
     @property
@@ -57,15 +55,18 @@ class MockDataPointRequest(object):
         Return the json from the request (from the canned data).
         """
 
-        resource_path = os.path.join(os.path.dirname(__file__), 'Resources', 
-            self.val, self.wx, self.item, self.params['res'],
-            self.feed + '.json')
+        resource_path = os.path.join(os.path.dirname(__file__),
+                                     'Resources',
+                                     self.val,
+                                     self.wx,
+                                     self.item,
+                                     self.params['res'],
+                                     self.feed + '.json')
 
         with open(resource_path, 'r') as file:
             content = json.load(file)
-        
-        return content
 
+        return content
 
 
 def get_json_from_path(path, query):
@@ -77,15 +78,20 @@ def get_json_from_path(path, query):
     splitpath = path.split('/')
     splitpath = list(filter(None, splitpath))
 
-    resource_path = os.path.join(os.path.dirname(__file__), 'Resources', 
-        splitpath[2], splitpath[3], splitpath[4], query['res'],
-        splitpath[-1] + '.json')
+    resource_path = os.path.join(os.path.dirname(__file__),
+                                 'Resources',
+                                 splitpath[2],
+                                 splitpath[3],
+                                 splitpath[4],
+                                 query['res'],
+                                 splitpath[-1] + '.json')
 
     with open(resource_path, 'rb') as file:
         content = file.read()
-    
+
     return content
-    
+
+
 def get_free_port():
     s = socket.socket(socket.AF_INET, type=socket.SOCK_STREAM)
     s.bind(('localhost', 0))
@@ -105,6 +111,6 @@ def start_mock_server():
 
 
 if __name__ == '__main__':
-    server = HTTPServer(('localhost', 8080), MockDataPointServer)
+    SERVER = HTTPServer(('localhost', 8080), MockDataPointServer)
     print('Starting server, use <Ctrl-C> to stop')
-    server.serve_forever()
+    SERVER.serve_forever()
